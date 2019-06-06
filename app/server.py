bd2daa438a030dc1b0d0419672d2fea230d409ad
Rodrigo Miranda <45832838+SrBliss@users.noreply.github.com>
2019-06-06 13:29:42 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -77,7 +77,8 @@ async def analyze(request):
     
     prediction = learn.predict(img, 55, temperature=0.75)
     print("prediction:", prediction)
-    return JSONResponse({'This is the poem': str(prediction)})
+    # return JSONResponse({'This is the poem': str(prediction)})
+    return JSONResponse({str(prediction)})
 
 
 if __name__ == '__main__':
